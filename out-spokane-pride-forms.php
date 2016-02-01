b666d4b7878787395ee81b339aa485a97a407fa6
Tony DeStefano <spokanetony@gmail.com>
2016-02-01 15:55:54 -0800

    Initial commit

---
+++ out-spokane-pride-forms.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: OutSpokane Pride Forms
+ * Plugin URI:
+ * Description: Custom plugin for all the OutSpokane forms
+ * Author: Tony DeStefano
+ * Author URI: https://www.facebook.com/TonyDeStefanoWeb
+ * Version: 1.0
+ * Text Domain: out-spokane-pride-forms
+ *
+ * Copyright 2016 Tony DeStefano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package OutSpokanePrideForms
+ * @author Tony DeStefano
+ * @version 1.0.0
+ */
